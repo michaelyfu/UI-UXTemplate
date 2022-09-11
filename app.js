78504a3e53872d0617573cc0669029918bcbe90c
michaelyfu <m888fu@gmail.com>
2022-09-11 17:39:04 -0400

    UIUX camp

--- app.js
+++ app.js
@@ -26,15 +26,24 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
+  const elements = fruitsArray.map((item) => {
+    const newEl = document.createElement("div");
+    newEl.innerHTML = item.name;
+    const existingEl = document.getElementById('cs1300-gallery');
+    existingEl.append(newEl);
+  });
 
-  // TODO: Create a new HTML element to display your data
-
-  // TODO: Append your new element to the page
-
+  // // TODO: Create a new HTML element to display your data
+  // const newEl = document.createElement("div");
+  // newEl.innerHTML = "test";
+  // // TODO: Append your new element to the page
+  // const existingEl = document.getElementById('cs1300-gallery');
+  // console.log(existingEl);
+  // existingEl.append(newEl);
 }
 
 // SAMPLE CODE of how to create and append a new HTML element to the page
